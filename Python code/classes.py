be89e632bfd56a59806f14832bbbafc09f12d44f
ottodejong <otto@DESKTOP-79LJ4PP.localdomain>
2023-06-06 12:09:24 +0200

    cleanup repo and added start to classes.py

---
+++ Python code/classes.py
@@ -0,0 +1,40 @@
+import csv
+
+class Grid:
+    """A class that stores all the data to play Crowther's Adventure. It loads
+    all the rooms from a game text-file. It also keeps track of the current room
+    and the player's inventory."""
+
+    def __init__(self, size: int):
+        """Creates the grid with all relevant information on coordinates."""
+
+        # initialize
+        self.size: int = size
+        self.grid: list[list[int]] = []
+
+
+        # import dictionaries from gamefile and Synonyms file
+        self.load_rooms(f"data/{game}Adv.dat")
+
+        self.load_synonyms()
+
+    def initialize_grid(self) -> None:
+        """Function that initializes a grid with the correct sizing. Fills every
+        spot with a 0."""
+
+        for height in self.size:
+            self.grid.append([])
+            for width in self.size:
+                self.grid[height].append(0)
+
+
+    def load_houses(self) -> None:
+        """Taken from https://earthly.dev/blog/csv-python/
+        """
+
+    with open("./bwq.csv", 'r') as file:
+
+      csvreader = csv.reader(file)
+
+      for row in csvreader:
+        print(row)
