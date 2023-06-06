c63f62510bca390ce0e95ead10d7700c417c7edc
ottodejong <otto.jan.de.jong@gmail.com>
2023-06-06 16:05:25 +0200

    work on classes.py and loaders.py, finished houses batteries and grid classes, demo for display function

--- python_code/classes.py
+++ python_code/classes.py
@@ -5,19 +5,16 @@ class Grid:
     all the rooms from a game text-file. It also keeps track of the current room
     and the player's inventory."""
 
-    def __init__(self, size: int):
+    def __init__(self, size: int, district: int):
         """Creates the grid with all relevant information on coordinates."""
 
         # initialize
+        self.district = district
         self.size: int = size
         self.grid: list[list[int]] = []
+        self.houses = []
+        self.batteries = []
         self.initialize_grid()
-
-
-        # import dictionaries from gamefile and Synonyms file
-        #self.load_houses(f"data/{game}Adv.dat")
-
-        #self.load_synonyms()
 
     def initialize_grid(self) -> None:
         """Function that initializes a grid with the correct sizing. Fills every
@@ -29,23 +26,74 @@ class Grid:
                 self.grid[height].append(0)
 
 
-    def process_houses(self, district: int) -> None:
+    def process_houses(self) -> None:
         """ Loads
         """
-        houses_data = load_houses(district)
+        houses_data = load_houses(self.district)
+
+        for house_row in houses_data:
+            x_location = int(house_row[0])
+            y_location = int(house_row[1])
+            max_output = house_row[2]
+
+            house = House(x_location, y_location, max_output)
+            self.houses.append(house)
+            self.grid[y_location][x_location] = 1
 
         return None
 
-    def process_batteries(self, district: int) -> None:
+    def process_batteries(self) -> None:
         """ Loads
         """
-        batteries_data = load_batteries(district)
+        batteries_data = load_batteries(self.district)
+
+        for battery_row in batteries_data:
+            x_location = int(battery_row[0])
+            y_location = int(battery_row[1])
+            max_capacity = battery_row[2]
+
+            battery = Battery(x_location, y_location, max_capacity)
+            self.batteries.append(battery)
+            self.grid[y_location][x_location] = 2
 
         return None
+
+    def print_grid(self):
+        for row in self.grid:
+            print(row)
+            print("")
+
+class House:
+    """Class for houses
+    autoincrement: https://stackoverflow.com/questions/1045344/how-do-you-create-an-incremental-id-in-a-python-class"""
+
+    house_id_counter = 0
+
+    def __init__(self, x_location: int, y_location: int, max_output: int):
+
+        self.id = House.house_id_counter
+        self.location = [x_location, y_location]
+        self.max_output = max_output
+        self.connected = False
+        self.battery_connection = None
+        House.house_id_counter += 1
+
+class Battery:
+    """Class for batteries"""
+
+    battery_id_counter = 0
+
+    def __init__(self, x_location: int, y_location: int, max_capacity: int):
+
+        self.id = Battery.battery_id_counter
+        self.location = [x_location, y_location]
+        self.max_capacity = max_capacity
+        self.house_connections = []
+        Battery.battery_id_counter += 1
+
 
 if __name__ == '__main__':
-    grid = Grid(10)
-    print(grid.grid)
-
-    yo = load_houses(1)
-    print(yo)
+    grid_1 = Grid(51,1)
+    grid_1.process_houses()
+    grid_1.process_batteries()
+    grid_1.print_grid()
--- python_code/loaders.py
+++ python_code/loaders.py
@@ -2,11 +2,13 @@ import csv
 
 def load_houses(district: int) -> list[list[str]]:
     """Loads the correct csv file.
-    Taken from https://earthly.dev/blog/csv-python/"""
+    Taken from https://earthly.dev/blog/csv-python/
+    skip header: https://linuxhint.com/skip-header-row-csv-python/"""
 
     with open(f"../data_grids/district_{district}/district-{district}_houses.csv", 'r') as file:
 
         csvreader = csv.reader(file)
+        next(csvreader)
 
         data_houses = []
         for row in csvreader:
@@ -21,9 +23,12 @@ def load_batteries(district: int) -> list[list[str]]:
     with open(f"../data_grids/district_{district}/district-{district}_batteries.csv", 'r') as file:
 
         csvreader = csv.reader(file)
+        next(csvreader)
 
         data_batteries = []
         for row in csvreader:
-            data_batteries.append(row)
+            # Position bestaat hier uit twee coordinaten
+            x,y = row[0].split(",")
+            data_batteries.append([x,y,row[1]])
 
         return data_batteries
