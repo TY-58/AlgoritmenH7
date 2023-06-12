e235cd3db335948b3f1df9f14ce53da0246be8a2
fredloth <frederieke_loth@live.nl>
2023-06-12 16:24:16 +0200

    started on checking path for other houses

--- python_code/cable_route.py
+++ python_code/cable_route.py
@@ -1,4 +1,5 @@
 from cable import Cable
+from house import House
 
 class Cable_route:
     """B"""
@@ -15,8 +16,8 @@ class Cable_route:
 
         start_location = house.location
         end_location = battery.location
-        print(start_location)
-        print(end_location)
+        #print(start_location)
+        #print(end_location)
 
         x_direction = abs(int(end_location[0] - start_location[0]))
         y_direction = abs(int(end_location[1] - start_location[1]))
@@ -24,32 +25,34 @@ class Cable_route:
         x_location = start_location[0]
         y_location = start_location[1]
 
-        if start_location[0] > end_location[0]:
+        self.check_surroundings(x_location, y_location)
 
+        if start_location[0] > end_location[0]:
             for x_counter in range(x_direction):
                 cable_route.append([x_location, y_location])
                 x_location -= 1
         else:
-
             for x_counter in range(x_direction):
                 cable_route.append([x_location, y_location])
                 x_location += 1
             
         if start_location[1] > end_location[1]:
-
             for y_counter in range(y_direction + 1):
                 cable_route.append([x_location, y_location])
                 y_location -= 1
             
         else:
-
             for y_counter in range(y_direction + 1):
                 cable_route.append([x_location, y_location])
                 y_location += 1
 
-        print(cable_route)
         return cable_route
 
+    def check_surroundings(self, x_location, y_location):
+
+        for combination in self.configuration:
+            house = combination[0].location
+            print(house)
 
     def lay_cables(self, configuration):
         """f"""
