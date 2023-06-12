4570c90ec1e21a22f9e05fd385ce1d614c7ce950
fredloth <frederieke_loth@live.nl>
2023-06-12 16:37:13 +0200

    prints locations where paths cross with houses

--- python_code/cable_route.py
+++ python_code/cable_route.py
@@ -25,34 +25,41 @@ class Cable_route:
         x_location = start_location[0]
         y_location = start_location[1]
 
-        self.check_surroundings(x_location, y_location)
-
         if start_location[0] > end_location[0]:
             for x_counter in range(x_direction):
                 cable_route.append([x_location, y_location])
                 x_location -= 1
+                self.check_surroundings(x_location, y_location)
         else:
             for x_counter in range(x_direction):
                 cable_route.append([x_location, y_location])
                 x_location += 1
+                self.check_surroundings(x_location, y_location)
             
         if start_location[1] > end_location[1]:
             for y_counter in range(y_direction + 1):
                 cable_route.append([x_location, y_location])
                 y_location -= 1
+                self.check_surroundings(x_location, y_location)
             
         else:
             for y_counter in range(y_direction + 1):
                 cable_route.append([x_location, y_location])
                 y_location += 1
+                self.check_surroundings(x_location, y_location)
 
         return cable_route
 
+    #prints statement where path crosses with houses
     def check_surroundings(self, x_location, y_location):
 
-        for combination in self.configuration:
-            house = combination[0].location
-            print(house)
+        for house in self.configuration:
+            if x_location == house[0].location[0]:
+                print(x_location, house[0].location[0])
+                print("path crosses house at x: ", x_location, "and y: ", house[0].location[1])
+            elif y_location == house[0].location[1]:
+                print(y_location, house[0].location[1])
+                print("path crosses house at x: ", house[0].location[0], "and y: ", y_location)
 
     def lay_cables(self, configuration):
         """f"""
