11039b09705c326b3663a174240e08788db116c9
fredloth <frederieke_loth@live.nl>
2023-06-18 16:09:07 +0200

    can succesfully swap houses without a battery

--- python_code/match_fred.py
+++ python_code/match_fred.py
@@ -2,7 +2,7 @@ import random
 import copy
 
 class Fred_configuration:
-    """ A Class for Random algorithm to match houses with batteries without exceeding max capacity. """
+    """ A Class that matches houses with batteries without exceeding max capacity. """
 
     def __init__(self, input_grid):
         self.grid = input_grid
@@ -15,12 +15,9 @@ class Fred_configuration:
         self.fit_unmatched()
 
     def make_configuration(self):
-
         for house in self.houses:
-
             battery = self.batteries[0]
             for b in self.batteries:
-                
                 if b.current_capacity > battery.current_capacity:
                     battery = b 
 
@@ -28,6 +25,7 @@ class Fred_configuration:
             if battery.current_capacity < house.max_output:
                 #print(house, "house does not fit anymore")
                 self.configuration.append([house, 0])
+                print("cannot match house ", house)
             
             else:
                 battery.current_capacity -= float(house.max_output)
@@ -37,12 +35,32 @@ class Fred_configuration:
         return self.configuration
             #assign to battery
 
-        #match houses with batteries based on capacity until fixed
+    #match houses with batteries based on capacity until fixed
     def fit_unmatched(self):
-        for match in self.configuration:
-            if match[1] == 0:
-                print("no match found for", match[0])
+        for combination in self.configuration:
+            if combination[1] == 0:
+                #print("no match found for", match[0])
+                battery = self.find_swap(combination[0])
+                #inserts the new battery for the combination
+                combination[1] = battery
 
+
+    #find houses to swap
+    def find_swap(self, house_1):
+        for combination in self.configuration:
+            capacity = 0
+            house = combination[0]
+            battery = combination[1]
+            if battery != 0:
+                capacity = battery.current_capacity + house.max_output
+            if capacity >= house_1.max_output and house.max_output < house_1.max_output:
+                print(house_1, "can switch with ", house)
+                #uncombines the found house
+                combination = [house, 0]
+                return battery
+        return 0
+
+        #house + battery capacity
         #rematches based on distance
         
         """
