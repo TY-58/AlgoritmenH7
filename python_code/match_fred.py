a696f46324f0f8587b7aa8a510560d402fa8f48c
fredloth <frederieke_loth@live.nl>
2023-06-19 15:12:48 +0200

    wrote match class to switch houses. Only with d1

--- python_code/match_fred.py
+++ python_code/match_fred.py
@@ -8,42 +8,39 @@ class Fred_configuration:
         self.grid = input_grid
         self.houses = input_grid.houses
         self.batteries = input_grid.batteries
-
-        #a list storing combination of house with battery
+        self.unmatched = 0
         self.configuration = []
-        self.make_configuration()
-        self.fit_unmatched()
-        self.repeat_finish()
-        self.show_combo()
+        self.add_houses_to_config()
+        #self.make_configuration()
+        self.repeat_to_finish()
+        self.do_calcs()
+        self.do_unmatch()
+        self.unmatch_houses()
+  
+    
 
-    def show_combo(self):
-        for combo in self.configuration:
-            print(combo)
-        for battery in self.batteries:
-            print(battery.current_capacity)
+    def repeat_to_finish(self):
+        """ Swaps and assigns batteries to houses until everything is matched. """
+        finished = self.check_unmatched()
+        while finished == False:
+            self.make_configuration()
+            self.find_unmatched()
+            finished = self.check_unmatched()
+        
+        #if finished == True:
+            #print("every house is matched")
+            #for battery in self.batteries:
+                #print(battery.current_capacity)
+
+    def add_houses_to_config(self):
+        """ Puts the houses in the configuration, unmatched to a battery. """
+        for house in self.houses:
+            self.configuration.append([house, 0])
 
     def make_configuration(self):
-        for house in self.houses:
-            battery = self.batteries[0]
-            for b in self.batteries:
-                if b.current_capacity > battery.current_capacity:
-                    battery = b 
-
-            #checks if the house still fits on a battery
-            if battery.current_capacity < house.max_output:
-                #print(house, "house does not fit anymore")
-                self.configuration.append([house, 0])
-                print("cannot match house ", house)
-            
-            else:
-                battery.current_capacity -= float(house.max_output)
-                self.configuration.append([house, battery])
-        
-        #print(self.configuration)
-        return self.configuration
-            #assign to battery
-    
-    def make_configuration2(self):
+        """ Checks self.configuration for unmatched houses and assigns to available batteries. """
+        count1 = 0
+        count2 = 0
         for combo in self.configuration:
             if combo[1] == 0:
                 house = combo[0]
@@ -54,63 +51,89 @@ class Fred_configuration:
 
                 #checks if the house still fits on a battery
                 if battery.current_capacity < house.max_output:
-                    #print(house, "house does not fit anymore")
-                    self.configuration.append([house, 0])
-                    print("cannot match house ", house)
-            
+                    count1 += 1
+                    pass
+                    #print("cannot match house ", house)
+
                 else:
+                    count2 += 1
+                    #print("before: ", battery.current_capacity)
                     battery.current_capacity -= float(house.max_output)
-                    self.configuration.append([house, battery])
+                    #print("after: ", battery.current_capacity)
+                    combo[1] = battery
 
-    #match houses with batteries based on capacity until fixed
-    def fit_unmatched(self):
-        for combination in self.configuration:
-            if combination[1] == 0:
-                #print("no match found for", match[0])
-                battery = self.find_swap(combination[0], combination[1])
-                #inserts the new battery for the combination
-                combination[1] = battery
+    def find_unmatched(self):
+        """ Finds unmatched houses, swaps with another battery """
+        for combo in self.configuration:
+            if combo[1] == 0:§
+                battery = self.find_swap(combo[0], combo[1])
+                combo[1] = battery
 
-
-    #find houses to swap swaps part and returns the battery to swap at other place
-    def find_swap(self, house_1, battery_1):
-        for combination in self.configuration:
+    def find_swap(self, house1, battery1):
+        #krijgt house1 en battery1 van plek waar house is gematched aan 0
+        print("battery1 is: ", battery1)
+        #for combination in self.configuration:
+        for i in range(len(self.configuration)):
+            #zoekt naar een batterij en huis combo waarvan battery + huis capaciteit genoeg is om te ruilen
+            house, battery = self.configuration[i]
             capacity = 0
-            house = combination[0]
-            battery = combination[1]
             if battery != 0:
                 capacity = battery.current_capacity + house.max_output
-            if capacity >= house_1.max_output and house.max_output < house_1.max_output:
-                print(house_1, "can switch with ", house)
-                #uncombines the found house
-                combination = [house, battery_1]
+
+            if capacity >= house1.max_output and house.max_output < house1.max_output:
+                #updates battery capacities
+                print("Capacity of battery to switch before: ", battery.current_capacity, "out: ", house.max_output," in: ", house1.max_output)
+                battery.current_capacity += float(house.max_output)
+                battery.current_capacity -= float(house1.max_output)
+                print("Capacity of battery to switch after: ", battery.current_capacity)
+                if battery1 != 0:
+                    battery1.current_capacity += float(house1.max_ouput)
+                    battery1.current_capacity -= float(house.max_ouput)
+                
+                print("battery1 before combining is: ", battery1)
+                self.configuration[i] = [house, battery1]
                 return battery
-        return 0
+        return 0 
 
+        #what to do if space spread over all capacities
 
-    def repeat_finish(self):
+    def do_calcs(self):
+        house_caps = 0
+        battery_caps = 0
+        battery_cur = 0
+        for house in self.houses:
+            house_caps += house.max_output
+
+        for battery in self.batteries:
+            battery_caps += float(battery.max_capacity)
+            battery_cur += float(battery.current_capacity)
+
+        print("house output: ", house_caps)
+        print("battery max cap: ", battery_caps)
+        print("battery cap left: ", battery_cur)
+
+    def do_unmatch(self):
+        count = 0
+        for combo in self.configuration:
+            count += 1
+            if combo[1] == 0:
+                print("house ", combo[0], "has no match. With output: ", combo[0].max_output)
+        print(count, "houses in list")
+ 
+    def unmatch_houses(self):
+        """ Take random houses to unmatch and make space for leftover houses"""
+        count = 0
+        for combo in self.configuration:
+            print(combo)
+            if combo[1] == 0:
+                count +=1
+        print(count,"unmatched houses")
+
+    def check_unmatched(self):
         for combo in self.configuration:
             if combo[1] == 0:
-                self.make_configuration2()
-                self.fit_unmatched
-                combo = 0
-    
+                return False
+        return True
 
-        #house + battery capacity
-        #rematches based on distance
-        
-        """
-        #van Otto
-        def try_configuration(self):
-        configuration = []
-        for house in self.sorted_houses:
-            batteries_sorted = []
-            for bat in self.grid.batteries:
-                batteries_sorted.append([bat, self.distance_to_battery(house, bat)])
-
-            #een lijst met tuples eerste tuple is batterij en tweede distance
-            batteries_sorted.sort(key=lambda a: a[1], reverse=False)
-
-        def distance_to_battery(self, house, battery):
-        return abs(house.location[0]- battery.location[0]) + abs(house.location[1] - battery.location[1])
-        """
+#na aantal iteraties opnieuw indelen
+#simulated annealing toepassen
