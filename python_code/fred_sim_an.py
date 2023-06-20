07b738b238c920d049d03573c8dbab309ecf197b
fredloth <frederieke_loth@live.nl>
2023-06-20 14:41:12 +0200

    part pseudocode for hillclimber type algorithm

--- python_code/fred_sim_an.py
+++ python_code/fred_sim_an.py
@@ -6,11 +6,30 @@ class HillClimber_Fred:
     """."""
 
     def __init__(self):
+
+    def copy_configuration(self):
+        """ Make a copy of the current configuration to manipulate. """
+
+    def score(self):
+        """ Assigns a score to the mutated configuration. """
+
+    def implement_score(self):
+        """ Decides if configuration should become mutated configuration. """
+
+    def save_scores(self):
+        """ Saves the scores of the mutation and"""
+
+    def stop_mutation(self):
+        """ Quits program if mutations do not improve configuration after X tries."""
         
     def mutate_match(self):
-        """mutate a single match."""
+        """ mutate a single match."""
 
-    
+    def find_match(self):
+        """ Find a match to mutate."""
+
+    def 
+
 
         #get a random match
         #match with new battery
