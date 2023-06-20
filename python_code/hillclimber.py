b9dd7e5f7ca362c610c4f9055cacf28cfafaf20f
fredloth <frederieke_loth@live.nl>
2023-06-20 14:52:13 +0200

    changes to names

--- python_code/hillclimber.py
+++ python_code/hillclimber.py
@@ -2,33 +2,43 @@ import random
 import copy
 from match_fred import Fred_configuration
 
-class HillClimber_Fred:
+class HillClimber:
     """."""
 
-    def __init__(self):
+    def __init__(self, input_config):
+        """ Loads an input configuration. """
+        self.input_config = input_config
+        self.current_config = input_config
 
     def copy_configuration(self):
         """ Make a copy of the current configuration to manipulate. """
+        pass
+
+    def find_match(self, configuration):
+        """ Find a match to mutate."""
+        match = random.choice(configuration)
+        print(match)
 
     def score(self):
         """ Assigns a score to the mutated configuration. """
+        pass
 
     def implement_score(self):
         """ Decides if configuration should become mutated configuration. """
+        pass
 
     def save_scores(self):
         """ Saves the scores of the mutation and"""
+        pass
 
     def stop_mutation(self):
         """ Quits program if mutations do not improve configuration after X tries."""
+        pass
         
     def mutate_match(self):
         """ mutate a single match."""
+        pass
 
-    def find_match(self):
-        """ Find a match to mutate."""
-
-    def 
 
 
         #get a random match
