bb5d94f3438ddf7759367836c4d4b1f06227d62a
fredloth <frederieke_loth@live.nl>
2023-06-27 13:54:51 +0200

    added iterations as input

--- code/algorithms/hillclimber.py
+++ code/algorithms/hillclimber.py
@@ -8,68 +8,56 @@ from .configurations.greedy_configuration import Greedy_configuration
 from code.classes.grid import Grid
 from code.visualisation.visualize import Gridplot
 
-MAX_STUCK: int = 5000
-
 class Hillclimber:
     """
     A class for performing a simple Hill Climber algorithm on a given grid.
     Takes a grid as input and mutates matches of houses and batteries from configuration list.
     Mutations are only performed when they are valid.
-    Performs mutations for the duration of a global variable MAX_STUCK.
+    Performs mutations for the duration of a a given number of iterations.
     """
 
 
-    def __init__(self, input_grid):
+    def __init__(self, grid, var):
         """
         Takes a grid as input and stores the grid.
+        Takes a variable for iterations and stores the iterations.
         Makes deepcopies of the input grid to make mutations without loss of input grid.
         """
-
-        self.input_grid = input_grid
-        self.input_config = input_grid.configuration
+        self.iterations = var
+        self.input_grid = grid
+        self.input_config = grid.configuration
         self.last_grid = copy.deepcopy(self.input_grid)
         self.last_config = self.last_grid.configuration
         self.current_grid = copy.deepcopy(self.input_grid)
         self.current_config = self.current_grid.configuration
-        self.current_score = input_grid.total_cost
+        self.first_score = grid.total_cost
+        self.current_score = grid.total_cost
         self.stuck = 0
 
 
     def do_mutate(self):
         """
         Calls for configuration mutations while score is improved at least once every X iterations.
-        X: Global Variable MAX_STUCK
+        X: a given number of iterations.
         """
 
-        print("score_old: ", self.current_score)
-        count = 0
-        count1 = 0
+        improvements = 0
+        
         # Performs mutations while at least 1 in every X iterations is an improvement
         while self.stop_mutation() == False:
-            count1 += 1
-            #print("before: ", self.current_config[0])
             new_config = self.mutate_match(self.current_config)
 
             # Replaces configuration in the grid with new_config
             self.current_grid.process_configuration_grid(new_config)
-            #print("after: ", new_config[0])
 
             score_new = self.score(new_config)
-            #print("new score: ", score_new)
-            #print("the", count1, "th time")
-
-            # Checks if the score is an improvement from last
-            improvement = self.implement_score(score_new)
+            improved = self.check_if_improved(score_new)
 
             # Saves the improvement
-            if improvement == True:
-                count += 1
+            if improved == True:
+                improvements += 1
                 print("improved after", self.stuck, "iterations")
-                print("Improved ", count, " times")
-                print("improved score: ", score_new)
-                # if count == 5 or count == 10 or count == 15 or count == 20 or count == 25 or count == 30 or count == 35 or count == 40 or count == 45 or count == 50 or count == 55:
-                #     visual = Gridplot(self.current_grid)
-                #     visual.make_plot()
+                print("improved costs: ", score_new)
                 self.last_grid = self.current_grid
                 self.last_config = self.current_config
                 self.current_score = score_new
@@ -78,16 +66,23 @@ class Hillclimber:
                 self.stuck = 0
 
             # Resets grid and configuration to last
-            elif improvement == False:
+            elif improved == False:
                 self.current_grid = copy.deepcopy(self.last_grid)
                 self.current_config = self.current_grid.configuration
 
-        print("score_new: ", self.current_score)
+        if improvements == 0:
+            print("There was no improvement made")
+            print("Costs: ", self.first_score)
+
+        else:
+            print("Improved ", improvements, " time(s)")
+            print("Old costs ", self.first_score)
+            print("New costs: ", self.current_score)
 
 
     def mutate_match(self, configuration):
         """
-        Mutates a single match in the given configuration and retursn the mutated configuration.
+        Mutates a single match in the given configuration and returns the mutated configuration.
         """
 
         # Finds valid mutations
@@ -102,7 +97,7 @@ class Hillclimber:
         match1[1] = match2[1]
         match2[1] = bat_switch
 
-        # Puts mutated matches in configuration
+        # Places mutated matches in configuration
         configuration[place1] = match1
         configuration[place2] = match2
 
@@ -111,7 +106,7 @@ class Hillclimber:
 
     def valid_mutation(self, match1, match2):
         """
-        Checks if the mutation is valid and returns Bool.
+        Checks if the mutation is valid and returns a Bool.
         """
 
         if match1[1] == match2[1]:
@@ -138,7 +133,7 @@ class Hillclimber:
         Finds a match to mutate. Returns place of match in configuration and match.
         """
 
-        # Picks a random location in the configuration list
+        # Takes a random location in the configuration list
         x = random.choice(range(len(configuration)))
         match_ = configuration[x]
 
@@ -172,14 +167,10 @@ class Hillclimber:
         for battery in self.current_grid.batteries:
             battery.house_connections = []
 
-        #print("configuration batteries ", configuration[0])
         # Reconnects houses to batteries according to new configuration
         self.current_grid.process_configuration_grid(configuration)
 
-        # Lays new cable routes for new configuration
-        #Shared_cable_route(self.current_grid, configuration)
-
-        #extended version
+        # 
         Shared_cable_extended(self.current_grid, configuration)
 
         # Calculates new cost
@@ -189,7 +180,7 @@ class Hillclimber:
         return score
 
 
-    def implement_score(self, score_new):
+    def check_if_improved(self, score_new):
         """
         Checks if the newly calculated score is better than the last score.
         Updates self.stuck if the new score is not an improvement.
@@ -208,10 +199,10 @@ class Hillclimber:
 
     def stop_mutation(self):
         """
-        Returns Bool if self.stuck exceeds global variable MAX_STUCK.
+        Returns Bool if self.stuck exceeds a given number of iterations.
         """
 
-        if self.stuck > MAX_STUCK:
+        if self.stuck > self.iterations:
             return True
 
         return False
