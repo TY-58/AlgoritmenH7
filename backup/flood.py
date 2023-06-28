fe59107f411f530196da61116ef2ddd4e71f21a3
fredloth <116084074+fredloth@users.noreply.github.com>
2023-06-28 16:21:49 +0200

    Delete backup directory
    Saved on personal computer and deleted backups

--- backup/flood.py
+++
@@ -1,9 +0,0 @@
-class Flood:
-    """ A class to implement flood type algorithm. """
-
-def __init__(self):
-
-    #scan area, which is closer to battery?
-        #go that direction until either another house or the battery
-        #branch of at a house in all directions that are favorable
-        
--- backup/helper.py
+++
@@ -1,5 +0,0 @@
-""" This code contains helper functions. """
-
-#this code is used in first Greedy class, does not exist anymore for now
-def function_sort_output(house):
-    return house.max_output
